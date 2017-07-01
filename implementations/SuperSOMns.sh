712471cf01c7964c5de6a98de60462cdae1d36bd
Friedrich Weber <fweber@posteo.de>
2017-07-01 11:07:38 +0200

    First step to integration of SuperSOMns

---
+++ implementations/SuperSOMns.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+SCRIPT_PATH=`dirname $0`
+source $SCRIPT_PATH/config.inc
+export JVMCI_BIN=$GRAAL_CORE_CMD
+export GRAAL_FLAGS=$GRAAL_HOSTED_FLAGS
+source $SCRIPT_PATH/superinstructions.inc
+pushd $SCRIPT_PATH/$SUPERSOMNS_PATH
+exec ./som -G "$@"
